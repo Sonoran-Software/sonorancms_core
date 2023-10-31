c0c1e9112ba8ee6887e34e9188dab29e89b526c0
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2023-10-31 12:28:52 -0400

    chore: Comment out additional unused code in addons.lua

--- sonorancms/server/addons.lua
+++ sonorancms/server/addons.lua
@@ -1,47 +1,47 @@
-local loadedPlugins = {}
+-- local loadedPlugins = {}
 
-local function RunAddonAutoUpdater(pluginName, latestVersion)
-	local f = LoadResourceFile(GetResourcePath(GetCurrentResourceName()) .. '/addonupdates/' .. pluginName .. '.zip')
-	if f ~= nil then
-		ExecuteCommand('stop ' .. pluginName)
-		os.remove(GetResourcePath(GetCurrentResourceName()) .. '/addonupdates/' .. pluginName .. '.zip')
-		os.remove(GetResourcePath(GetCurrentResourceName()) .. '/run.lock')
-	end
-	if FileExists(GetResourcePath(GetCurrentResourceName()) .. '/config.lock') then
-		os.remove(GetResourcePath(GetCurrentResourceName()) .. '/config.lock')
-	end
-	local pluginRepo = GetResourceMetadata(pluginName, 'git_repo')
-	local releaseUrl = pluginRepo .. '/archive/refs/tags/latest.zip'
-	PerformHttpRequest(releaseUrl, function(code, data, _)
-		if code == 200 then
-			local savePath = GetResourcePath(GetCurrentResourceName()) .. '/addonupdates/' .. pluginName .. '.zip'
-			local unzipPath = GetResourcePath(pluginName)
-			local f = assert(io.open(savePath, 'wb'))
-			f:write(data)
-			f:close()
-			Utilities.Logging.logInfo('Saved file...')
-			exports[GetCurrentResourceName()]:UnzipFile(savePath, unzipPath, pluginName)
-		else
-			Utilities.Logging.logWarn(('Failed to download from %s: %s %s'):format(releaseUrl, code, data))
-		end
-	end, 'GET')
-end
+-- local function RunAddonAutoUpdater(pluginName, latestVersion)
+-- 	local f = LoadResourceFile(GetResourcePath(GetCurrentResourceName()) .. '/addonupdates/' .. pluginName .. '.zip')
+-- 	if f ~= nil then
+-- 		ExecuteCommand('stop ' .. pluginName)
+-- 		os.remove(GetResourcePath(GetCurrentResourceName()) .. '/addonupdates/' .. pluginName .. '.zip')
+-- 		os.remove(GetResourcePath(GetCurrentResourceName()) .. '/run.lock')
+-- 	end
+-- 	if FileExists(GetResourcePath(GetCurrentResourceName()) .. '/config.lock') then
+-- 		os.remove(GetResourcePath(GetCurrentResourceName()) .. '/config.lock')
+-- 	end
+-- 	local pluginRepo = GetResourceMetadata(pluginName, 'git_repo')
+-- 	local releaseUrl = pluginRepo .. '/archive/refs/tags/latest.zip'
+-- 	PerformHttpRequest(releaseUrl, function(code, data, _)
+-- 		if code == 200 then
+-- 			local savePath = GetResourcePath(GetCurrentResourceName()) .. '/addonupdates/' .. pluginName .. '.zip'
+-- 			local unzipPath = GetResourcePath(pluginName)
+-- 			local f = assert(io.open(savePath, 'wb'))
+-- 			f:write(data)
+-- 			f:close()
+-- 			Utilities.Logging.logInfo('Saved file...')
+-- 			exports[GetCurrentResourceName()]:UnzipFile(savePath, unzipPath, pluginName)
+-- 		else
+-- 			Utilities.Logging.logWarn(('Failed to download from %s: %s %s'):format(releaseUrl, code, data))
+-- 		end
+-- 	end, 'GET')
+-- end
 
-exports('unzipAddonCompleted', function(success, error, pluginName)
-	if success then
-		if GetNumPlayerIndices() > 0 then
-			pendingRestart = true
-			Utilities.Logging.logInfo('Delaying auto-update until server is empty.')
-			return
-		end
-		Utilities.Logging.logWarn('Auto-restarting addon...')
-		Citizen.Wait(5000)
-		ExecuteCommand('refresh')
-		ExecuteCommand('ensure ' .. pluginName)
-	else
-		Utilities.Logging.logError('Failed to download addon update for ' .. pluginName .. ' Error: ' .. json.encode(error))
-	end
-end)
+-- exports('unzipAddonCompleted', function(success, error, pluginName)
+-- 	if success then
+-- 		if GetNumPlayerIndices() > 0 then
+-- 			pendingRestart = true
+-- 			Utilities.Logging.logInfo('Delaying auto-update until server is empty.')
+-- 			return
+-- 		end
+-- 		Utilities.Logging.logWarn('Auto-restarting addon...')
+-- 		Citizen.Wait(5000)
+-- 		ExecuteCommand('refresh')
+-- 		ExecuteCommand('ensure ' .. pluginName)
+-- 	else
+-- 		Utilities.Logging.logError('Failed to download addon update for ' .. pluginName .. ' Error: ' .. json.encode(error))
+-- 	end
+-- end)
 
 RegisterNetEvent('SonoranCMS::Plugins::Loaded', function(pluginName)
 	if GetResourceState(pluginName) == 'started' then
