6abe5e434c20d830a4dfbe89c5400c8dfe97b01f
Jordan.#2139 <jordansilva213@gmail.com>
2024-01-23 14:05:35 -0500

    Refactor UnzipFile function to handle JSON config updates

--- sonorancms/server/util/unzip.js
+++ sonorancms/server/util/unzip.js
@@ -76,57 +76,39 @@ function findChanges(existingConfig, defaultConfig, basePath = '') {
 }
 
 
-exports('UnzipFile', (file, dest, type) => {
-	if (type === "core") {
-		try {
-			fs.createReadStream(file).pipe(unzipper.Extract({ path: dest }).on('close', () => {
-				// New logic to handle the JSON config update
-				const moduleDirectories = ['ace-permissions', 'clockin', 'jobsync', 'whitelist']; // Add all module directories here
-				let globalChanges = [];
-				moduleDirectories.forEach(moduleName => {
-					const configPath = path.join(dest, moduleName, `${moduleName}_config.json`);
-					const distConfigPath = path.join(dest, moduleName, `${moduleName}_config.dist.json`);
+exports('UnzipFile', (file, dest) => {
+	try {
+		fs.createReadStream(file).pipe(unzipper.Extract({ path: dest }).on('close', () => {
+			// New logic to handle the JSON config update
+			const moduleDirectories = ['ace-permissions', 'clockin', 'jobsync', 'whitelist']; // Add all module directories here
+			let globalChanges = [];
+			moduleDirectories.forEach(moduleName => {
+				const configPath = path.join(dest, moduleName, `${moduleName}_config.json`);
+				const distConfigPath = path.join(dest, moduleName, `${moduleName}_config.dist.json`);
 
-					if (fs.existsSync(distConfigPath)) {
-						if (!fs.existsSync(configPath)) {
-							// If the regular config doesn't exist, rename the dist config file to regular config file
-							fs.renameSync(distConfigPath, configPath);
-							console.log(`[${moduleName}] No existing config found. Renamed dist config to regular config.`);
-						} else {
-							// If the regular config exists, proceed with the merge
-							let existingConfig = JSON.parse(fs.readFileSync(configPath));
-							const distConfig = JSON.parse(fs.readFileSync(distConfigPath));
-							const mergedConfig = deepMerge(existingConfig, distConfig);
-							const changesArray = findChanges(existingConfig, distConfig);
-							if (changesArray.length > 0) {
-								fs.writeFileSync(configPath, JSON.stringify(mergedConfig, null, 2));
-								console.log(`[${moduleName}] Config updated with changes:`, changesArray);
-								globalChanges = globalChanges.concat(changesArray.map(change => `[${moduleName}] ${change}`));
-							}
+				if (fs.existsSync(distConfigPath)) {
+					if (!fs.existsSync(configPath)) {
+						// If the regular config doesn't exist, rename the dist config file to regular config file
+						fs.renameSync(distConfigPath, configPath);
+						console.log(`[${moduleName}] No existing config found. Renamed dist config to regular config.`);
+					} else {
+						// If the regular config exists, proceed with the merge
+						let existingConfig = JSON.parse(fs.readFileSync(configPath));
+						const distConfig = JSON.parse(fs.readFileSync(distConfigPath));
+						const mergedConfig = deepMerge(existingConfig, distConfig);
+						const changesArray = findChanges(existingConfig, distConfig);
+						if (changesArray.length > 0) {
+							fs.writeFileSync(configPath, JSON.stringify(mergedConfig, null, 2));
+							console.log(`[${moduleName}] Config updated with changes:`, changesArray);
+							globalChanges = globalChanges.concat(changesArray.map(change => `[${moduleName}] ${change}`));
 						}
 					}
-				});
-				exports[GetCurrentResourceName()].unzipCoreCompleted(true, globalChanges.length > 0 ? globalChanges : 'nil');
-			}));
-		} catch (ex) {
-			exports[GetCurrentResourceName()].unzipCoreCompleted(false, ex);
-		}
-	} else {
-		try {
-			let tempFolder = GetResourcePath(GetCurrentResourceName()) + '/addonupdates/' + type
-			if (fs.existsSync(tempFolder)) { rmdirRecursive(tempFolder) }
-			fs.mkdirSync(tempFolder)
-			fs.createReadStream(file).pipe(unzipper.Extract({ path: tempFolder }).on('close', () => {
-				const folder = fs.readdirSync(tempFolder);
-				if (folder[0].includes('-latest')) {
-					moveFiles(tempFolder + '/' + folder, dest)
-					rmdirRecursive(tempFolder);
-					exports[GetCurrentResourceName()].unzipAddonCompleted(true, 'nil', type);
 				}
-			}))
-		} catch (ex) {
-			exports[GetCurrentResourceName()].unzipAddonCompleted(false, ex, type);
-		}
+			});
+			exports[GetCurrentResourceName()].unzipCoreCompleted(true, globalChanges.length > 0 ? globalChanges : 'nil');
+		}));
+	} catch (ex) {
+		exports[GetCurrentResourceName()].unzipCoreCompleted(false, ex?.message);
 	}
 });
 
